add0c1edbc943324b6593aa2c26cfcd5c76104d5
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-12-21 15:40:50 +0530

    Reading from CSV File and writing Json  File

--- src/main/java/com/brigdelabz/OpenCSVAndGsonTester.java
+++ src/main/java/com/brigdelabz/OpenCSVAndGsonTester.java
@@ -1,2 +1,44 @@
-package com.brigdelabz;public class OpenCSVAndGsonTester {
+package com.brigdelabz;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class OpenCSVAndGsonTester {
+    private static final String SAMPLE_CSV_FILE_PATH="./src/main/resources/users.csv";
+    private static final String SAMPLE_JSON_FILE_PATH="./src/main/resources/users.json";
+    public static void main(String[] args){
+        try{
+            Reader reader= Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+            CsvToBeanBuilder<CSVUser>csvToBeanBuilder=new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(CSVUser.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<CSVUser>csvToBean=csvToBeanBuilder.build();
+            List<CSVUser>csvUsers=csvToBean.parse();
+            Gson gson=new Gson();
+            String json=gson.toJson(csvUsers);
+            FileWriter writer=new FileWriter(SAMPLE_JSON_FILE_PATH);
+            writer.write(json);
+            writer.close();
+            BufferedReader br=new BufferedReader(new FileReader(SAMPLE_JSON_FILE_PATH));
+            CSVUser[] usrObj=gson.fromJson(br,CSVUser[].class);
+            List<CSVUser> csvUserList= Arrays.asList(usrObj);
+
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
+
